934ed44810ad1132825478173241d380927fbac3
Yovanovid <49575883+Yovanovid@users.noreply.github.com>
2019-06-09 22:10:31 -0500

    Delete testthat.R

--- tests/testthat.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-library(farsfunction)
-
-test_check("farsfunction")
-
-expect_that(fars_map_state(00,2015), throws_error("invalid STATE number: 0"))
-
-expect_that(fars_read("productions.csv"), throws_error("file 'productions.csv' does not exist"))
